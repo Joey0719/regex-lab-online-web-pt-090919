91f3994bbf4c126a2f368814265fe3d79b469a2b
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-09-17 21:35:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  word.match(/^[aeiouAEIOU]\w+/) ? true : false 
+  word.match(/^[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -13,9 +13,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].+[\.!?]$/) ? true : false 
+  text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-  phone.match(/([0-9] *?){10} | (\[0-9]{3}\)(([0-9]{3}-[0-9]{4}) | [0-9]{7})\b) ? true : false 
- end
+  phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
+end
